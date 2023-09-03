d1436dd521937df4437bcdd26c2e2ad14a8b367b
anishazahan <anishazahan13@gmail.com>
2023-09-04 00:00:03 +0600

    database connecred successfully

---
+++ controllers/authControllers.js
@@ -0,0 +1,30 @@
+class authControllers {
+  admin_login = async (req, res) => {
+    console.log(req.body);
+    // const { email, password } = req.body
+    // try {
+    //     const admin = await adminModel.findOne({ email }).select('+password')
+    //     if (admin) {
+    //         const match = await bcrpty.compare(password, admin.password)
+    //         if (match) {
+    //             const token = await createToken({
+    //                 id: admin.id,
+    //                 role: admin.role
+    //             })
+    //             res.cookie('accessToken', token, {
+    //                 expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
+    //             })
+    //             responseReturn(res, 200, { token, message: 'Login success' })
+    //         } else {
+    //             responseReturn(res, 404, { error: "Password wrong" })
+    //         }
+    //     } else {
+    //         responseReturn(res, 404, { error: "Email not found" })
+    //     }
+    // } catch (error) {
+    //     responseReturn(res, 500, { error: error.message })
+    // }
+  };
+}
+
+module.exports = new authControllers();
--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const { dbConnect } = require("./utiles/db");
+
+//------- global middleware----------
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:3001"],
+    credentials: true,
+  })
+);
+
+//------- production routes ----------
+
+app.use("/api", require("./routes/authRoutes"));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+const port = process.env.PORT;
+dbConnect();
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
