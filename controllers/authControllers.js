e06987e0cb13c410bcb6de6c7b9c25069e7f986f
anishazahan <anishazahan13@gmail.com>
2023-09-04 05:33:21 +0600

    admin login controller create

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,31 +1,36 @@
 const adminModel = require("../models/adminModel");
+const { responseReturn } = require("../utiles/response");
+const bcrpty = require("bcrypt");
+const { createToken } = require("../utiles/tokenCreate");
+const jwt = require("jsonwebtoken");
 
 class authControllers {
   admin_login = async (req, res) => {
-    console.log(req.body);
+    // console.log(req.body);
     const { email, password } = req.body;
     try {
       const admin = await adminModel.findOne({ email }).select("+password");
-      console.log(admin, "admin result-------");
-      //   if (admin) {
-      //     const match = await bcrpty.compare(password, admin.password);
-      //     if (match) {
-      //       const token = await createToken({
-      //         id: admin.id,
-      //         role: admin.role,
-      //       });
-      //       res.cookie("accessToken", token, {
-      //         expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-      //       });
-      //       responseReturn(res, 200, { token, message: "Login success" });
-      //     } else {
-      //       responseReturn(res, 404, { error: "Password wrong" });
-      //     }
-      //   } else {
-      //     responseReturn(res, 404, { error: "Email not found" });
-      //   }
+      //   console.log(admin, "admin result-------");
+      if (admin) {
+        const match = await bcrpty.compare(password, admin.password);
+        // console.log(match);
+        if (match) {
+          const token = await createToken({
+            id: admin.id,
+            role: admin.role,
+          });
+          res.cookie("accessToken", token, {
+            expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+          });
+          responseReturn(res, 200, { token, message: "Login success" });
+        } else {
+          responseReturn(res, 404, { error: "Password wrong" });
+        }
+      } else {
+        responseReturn(res, 404, { error: "Email not found" });
+      }
     } catch (error) {
-      //   responseReturn(res, 500, { error: error.message });
+      responseReturn(res, 500, { error: error.message });
     }
   };
 }
