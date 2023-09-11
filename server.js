742f4647fad0e3d7ab113f2920a09af4ec54073f
anishazahan <anishazahan13@gmail.com>
2023-09-11 20:53:09 +0600

    authorder model create

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.use(
 //------- production routes ----------
 
 app.use("/api/home", require("./routes/home/homeRoutes"));
+app.use("/api", require("./routes/order/orderRoutes"));
 app.use("/api", require("./routes/authRoutes"));
 app.use("/api", require("./routes/home/cardRoutes"));
 app.use("/api", require("./routes/home/customerAuthRoutes"));
