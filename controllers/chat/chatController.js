78f94728a305b0a350ee3c1f24126629c8d8867f
anishazahan <anishazahan13@gmail.com>
2023-09-13 13:23:47 +0600

    logout functionality admin and seller

--- controllers/chat/chatController.js
+++ controllers/chat/chatController.js
@@ -128,58 +128,58 @@ class chatController {
     }
   };
 
-  //   customer_message_add = async (req, res) => {
-  //     const { userId, text, sellerId, name } = req.body;
-  //     try {
-  //       const message = await sellerCustomerMessage.create({
-  //         senderId: userId,
-  //         senderName: name,
-  //         receverId: sellerId,
-  //         message: text,
-  //       });
+  customer_message_add = async (req, res) => {
+    const { userId, text, sellerId, name } = req.body;
+    try {
+      const message = await sellerCustomerMessage.create({
+        senderId: userId,
+        senderName: name,
+        receverId: sellerId,
+        message: text,
+      });
 
-  //       const data = await sellerCustomerModel.findOne({ myId: userId });
-  //       let myFriends = data.myFriends;
-  //       let index = myFriends.findIndex((f) => f.fdId === sellerId);
-  //       while (index > 0) {
-  //         let temp = myFriends[index];
-  //         myFriends[index] = myFriends[index - 1];
-  //         myFriends[index - 1] = temp;
-  //         index--;
-  //       }
-  //       await sellerCustomerModel.updateOne(
-  //         {
-  //           myId: userId,
-  //         },
-  //         {
-  //           myFriends,
-  //         }
-  //       );
-  //       const data1 = await sellerCustomerModel.findOne({ myId: sellerId });
-  //       let myFriends1 = data1.myFriends;
-  //       let index1 = myFriends1.findIndex((f) => f.fdId === userId);
+      const data = await sellerCustomerModel.findOne({ myId: userId });
+      let myFriends = data.myFriends;
+      let index = myFriends.findIndex((f) => f.fdId === sellerId);
+      while (index > 0) {
+        let temp = myFriends[index];
+        myFriends[index] = myFriends[index - 1];
+        myFriends[index - 1] = temp;
+        index--;
+      }
+      await sellerCustomerModel.updateOne(
+        {
+          myId: userId,
+        },
+        {
+          myFriends,
+        }
+      );
+      const data1 = await sellerCustomerModel.findOne({ myId: sellerId });
+      let myFriends1 = data1.myFriends;
+      let index1 = myFriends1.findIndex((f) => f.fdId === userId);
 
-  //       while (index1 > 0) {
-  //         let temp1 = myFriends1[index1];
-  //         myFriends1[index1] = myFriends[index1 - 1];
-  //         myFriends1[index1 - 1] = temp1;
-  //         index1--;
-  //       }
+      while (index1 > 0) {
+        let temp1 = myFriends1[index1];
+        myFriends1[index1] = myFriends[index1 - 1];
+        myFriends1[index1 - 1] = temp1;
+        index1--;
+      }
 
-  //       await sellerCustomerModel.updateOne(
-  //         {
-  //           myId: sellerId,
-  //         },
-  //         {
-  //           myFriends1,
-  //         }
-  //       );
+      await sellerCustomerModel.updateOne(
+        {
+          myId: sellerId,
+        },
+        {
+          myFriends1,
+        }
+      );
 
-  //       responseReturn(res, 201, { message });
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   };
+      responseReturn(res, 201, { message });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   get_customers = async (req, res) => {
     const { sellerId } = req.params;
